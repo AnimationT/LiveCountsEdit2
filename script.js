b0e388a363395aed47431ade679545d1e8fb4cf7
AnimationT <86012532+AnimationT@users.noreply.github.com>
2022-07-02 08:46:59 +0000

    Selectable amount type

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 var subCount = 0;
 var chanName = "LiveCountsEdit Channel";
+var amType = "things";
 
 $("h1").click(function(){
    chanName = window.prompt("Enter the new channel name:" , chanName);
     $("h1").text(chanName);
 });
+
+$("h2").click(function(){
+    amType = window.prompt("What amount are you measuring?" , amType);
+     $("h2").text(amType);
+ });
 
 $("#subCountButton").click(function(){
     subCount = parseInt(document.getElementById("subCountInput").value);
